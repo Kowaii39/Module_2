a81718ad413c09eadb8d8ff21b3d6189b9382fe2
Дмитрий <overlord381@gmail.com>
2024-11-26 13:34:05 +0200

    upload

---
+++ module_2_3.py
@@ -0,0 +1,10 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+n = 0
+while n < len(my_list):
+    num = my_list[n]
+    n = n + 1
+    if num == 0:
+        continue
+    elif num < 0:
+        break
+    print(num)
